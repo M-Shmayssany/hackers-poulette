9a6dcced38247e78082fdfb0f4a3a21453373d7b
M-Shmayssany <m.shmayssany@gmail.com>
2020-08-27 12:33:51 +0200

    test the site

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php
-echo "hello World!";
-?>
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Title Page</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.0/css/bulma.css"/>
+    </head>
+    <body>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">
+                Hello World
+            </h1>
+            <p class="subtitle">
+                My first website with <strong>Bulma</strong>!
+            </p>
+            <a class="button is-primary is-outlined">Button</a>
+        </div>
+    </section>
+    </body>
+</html>
